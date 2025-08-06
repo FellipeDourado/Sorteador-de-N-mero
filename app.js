b54883c28907a05240c1c4cefe795b3270bf3e14
FellipeDourado <fellipecamposdourado23@hotmail.com>
2025-08-06 13:36:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
-    let até = parseInt(document.getElementById('até').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
     let sorteados = [];
     let numero;
 
     for (let i = 0; i < quantidade; i++) {
-        numero = obterNúmeroAleatorio(de, até);
+        numero = obterNúmeroAleatorio(de, ate);
 
         while (sorteados.includes(numero)) {
-            numero = obterNúmeroAleatorio(de, até);
+            numero = obterNúmeroAleatorio(de, ate);
         }
 
         sorteados.push(numero);
@@ -37,7 +37,7 @@ function AtivarBotaoReiniciar() {
 function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
-    document.getElementById('até').value = '';
+    document.getElementById('ate').value = '';
     document.getElementById('resultado').innerHTML =
         '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
 
@@ -45,3 +45,4 @@ function reiniciar() {
     botao.classList.remove('container__botao');
     botao.classList.add('container__botao-desabilitado');
 }
+
